19c32eae362f07590dcb832e39bfbead9373f3a6
z411 <electrik.persona@gmail.com>
2016-10-14 18:16:17 -0300

    Migrated hooks to the new multi-hook system

--- docs/hooks/monthly.py
+++ docs/hooks/monthly.py
@@ -2,9 +2,8 @@
 #
 # This hook sends episode updates to monthly.moe and marks them as watched.
 #
-# To use, copy this file to ~/.trackma/hook.py and fill in the access token.
+# To use, copy this file to ~/.trackma/hooks/monthly.py and fill in the access token.
 
-##### Hook start #####
 ACCESS_TOKEN = ""
 
 if not ACCESS_TOKEN:
@@ -16,7 +15,7 @@ import trackma.utils as utils
 MONTHLY_URL = "http://www.monthly.moe/api"
 HEADERS = {'User-Agent': 'Trackma/{}'.format(utils.VERSION)}
 
-def monthly_send(engine, show):
+def episode_changed(engine, show):
     api_name = engine.api_info['name']
     if api_name != "MyAnimeList":
         engine.msg.warn('Monthly.moe', "This currently only works with MyAnimeList.")
@@ -37,8 +36,4 @@ def monthly_send(engine, show):
     if not json_data['success']:
         engine.msg.warn('Monthly.moe', "Problem updating episode.")
 
-##### HOOK END #####
-
-def episode_changed(engine, show):
-    monthly_send(engine, show)
 
