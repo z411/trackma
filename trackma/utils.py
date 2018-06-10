454d8ad73b73f1ee936e697714eca861f83da4b7
z411 <z411@omaera.org>
2018-06-10 00:45:13 -0400

    Made full path scan optional and disabled by default (fixes #333)

--- trackma/utils.py
+++ trackma/utils.py
@@ -284,6 +284,7 @@ config_defaults = {
     'autosend_size': 5,
     'autosend_at_exit': True,
     'library_autoscan': True,
+    'library_full_path': False,
     'scan_whole_list': False,
     'debug_disable_lock': True,
     'auto_status_change': True,
