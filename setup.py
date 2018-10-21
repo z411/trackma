d7ad83737d4bc4a97fc98813aabf23af928d26ca
Anjan Momi <anjan@momi.ca>
2018-10-20 18:45:46 -0700

    ignore translate compile files, setup now compiles files before install

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup, find_packages
 from trackma import utils
+import os
+import subprocess
 
 try:
     LONG_DESCRIPTION = open("README.rst").read()
@@ -15,6 +17,26 @@ EXTRA_REQUIREMENTS = {
     'GTK': ['pygobject'],
     'Qt': [],
 }
+
+
+def create_mo_files():
+    data_files = []
+    localedir = 'locales'
+    po_dirs = [localedir + '/' + l + '/LC_MESSAGES/'
+               for l in next(os.walk(localedir))[1]]
+    for d in po_dirs:
+        mo_files = []
+        po_files = [f
+                    for f in next(os.walk(d))[2]
+                    if os.path.splitext(f)[1] == '.po']
+        for po_file in po_files:
+            filename, extension = os.path.splitext(po_file)
+            mo_file = filename + '.mo'
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(d + po_file, d + mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo_files.append(d + mo_file)
+        data_files.append((d, mo_files))
+    return data_files
 
 setup(
     name=NAME,
@@ -52,5 +74,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Operating System :: POSIX',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    ]
+    ],
+    data_files = create_mo_files(),
     )
