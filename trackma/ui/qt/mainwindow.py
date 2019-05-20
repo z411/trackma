c446a33f7236c9d471bb0fd7329a7ace6a9c957d
z411 <z411@omaera.org>
2019-05-20 18:29:56 -0400

    Qt: Moved column restore to its function

--- trackma/ui/qt/mainwindow.py
+++ trackma/ui/qt/mainwindow.py
@@ -763,9 +763,14 @@ class MainWindow(QMainWindow):
             self.view.horizontalHeader().setResizeMode(2, QHeaderView.Fixed)
             self.view.horizontalHeader().setResizeMode(3, QHeaderView.Fixed)
 
-        self.view.horizontalHeader().resizeSection(2, 70)
-        self.view.horizontalHeader().resizeSection(3, 55)
-        self.view.horizontalHeader().resizeSection(4, 100)
+        # Recover column state
+        if self.config['remember_columns'] and isinstance(self.api_config['columns_state'], str):
+            state = QtCore.QByteArray(base64.b64decode(self.api_config['columns_state']))
+            self.view.horizontalHeader().restoreState(state)
+        else:
+            self.view.horizontalHeader().resizeSection(2, 70)
+            self.view.horizontalHeader().resizeSection(3, 55)
+            self.view.horizontalHeader().resizeSection(4, 100)
 
     def _select_show(self, show):
         # Stop any running image timer
@@ -1358,11 +1363,6 @@ class MainWindow(QMainWindow):
             self._rebuild_view()
             self._init_view()
             self._recalculate_counts()
-
-            # Recover column state
-            if self.config['remember_columns'] and isinstance(self.api_config['columns_state'], str):
-                state = QtCore.QByteArray(base64.b64decode(self.api_config['columns_state']))
-                self.view.horizontalHeader().restoreState(state)
 
             self.s_show_selected(None)
 
