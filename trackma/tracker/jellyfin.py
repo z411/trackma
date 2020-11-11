942d77f667111e1d53808a521803e3c3a4e31829
Lachlan Slape <slapelachie@gmail.com>
2020-11-11 23:20:11 +1000

    Improve jellyfin tracker

--- trackma/tracker/jellyfin.py
+++ trackma/tracker/jellyfin.py
@@ -17,7 +17,6 @@
 import time
 import requests
 
-import trackma.utils as utils
 from trackma.tracker import tracker
 
 NOT_RUNNING = 0
@@ -38,73 +37,45 @@ class JellyfinTracker(tracker.TrackerBase):
         self.api_key = self.config['jellyfin_api_key']
         self.username = self.config['jellyfin_user']
         self.status_log = [None, None]
+
         super().__init__(messenger, tracker_list, config, watch_dirs, redirections)
-
-    def get_jellyfin_status(self, session_state):
-        # returns the plex status of the first active session
-        #try:
-        active = session_state
-
-        if active:
-            return ACTIVE
-        else:
-            return IDLE
-        #except urllib.request.URLError as e:
-        #    if e.code == 401:
-        #        return CLAIMED
-        #    else:
-        #        return NOT_RUNNING
-
-    def playing_file(self, session_info):
-        # returns the filename of the currently playing file
-        if self.get_jellyfin_status(session_info[0]) == IDLE:
-            return None
-
-        if not session_info[1]:
-            state = PLAYING
-        else:
-            state = PAUSED
-
-        name = session_info[2]
-
-        return (name, state)
 
     def observe(self, config, watch_dirs):
         self.msg.info(self.name, "Using Jellyfin.")
-        session_info = self._get_sessions_info()
 
         while self.active:
             session_info = self._get_sessions_info()
-            self.status_log.append(self.get_jellyfin_status(session_info[0]))
+            self.status_log.append(session_info['status'])
 
             if self.status_log[-1] == ACTIVE or self.status_log[-1] == IDLE:
                 if self.status_log[-1] == IDLE and self.status_log[-2] == NOT_RUNNING:
                     self.msg.info(self.name, "Reconnected to Jellyfin.")
                     self.wait_s = config['tracker_update_wait_s']
+
                 try:
-                    player = self.playing_file(session_info)
-                    (state, show_tuple) = self._get_playing_show(player[0])
-                            
-                    self.view_offset = int(session_info[3])
+                    (state, show_tuple) = self._get_playing_show(session_info['file_name'])
+
+                    self.view_offset = int(session_info['view_offset'])
 
                     self.update_show_if_needed(state, show_tuple)
 
-                    if player[1] == PAUSED:
+                    if session_info['state'] == PAUSED:
                         self.pause_timer()
-                    elif player[1] == PLAYING:
+                    elif session_info['state'] == PLAYING:
                         self.resume_timer()
 
-                except:
+                except (IndexError,TypeError):
                     if self.status_log[-1] == IDLE:
                         self.last_filename = None
                         self.update_show_if_needed(0, None)
                     else:
                         pass
+
             elif self.status_log[-1] == CLAIMED and self.status_log[-2] == CLAIMED:
                 self.msg.warn(
-                    self.name, "Claimed Plex Media Server, login in the settings and restart trackma.")
+                    self.name, "Claimed Jellyfin, login in the settings and restart trackma.")
             elif self.status_log[-1] == NOT_RUNNING and self.status_log[-2] == NOT_RUNNING:
-                self.msg.warn(self.name, "Plex Media Server is not running.")
+                self.msg.warn(self.name, "Jellyfin is not running.")
 
             del self.status_log[0]
 
@@ -112,22 +83,35 @@ class JellyfinTracker(tracker.TrackerBase):
             time.sleep(config['tracker_interval'])
 
     def _get_sessions_info(self):
-        # Get the required info from the /status/sessions url
         session_url = "http://"+self.host_port+"/Sessions?api_key={}".format(self.api_key)
-        response = requests.get(session_url).json()
-        for session in response:
+
+        info = {
+            "status": NOT_RUNNING,
+            "state": PAUSED,
+            "file_name": None,
+            "view_offset": None
+        }
+
+        try:
+            response = requests.get(session_url)
+            response_json = response.json()
+        except requests.exceptions.ConnectionError:
+            return info
+
+        for session in response_json:
             if not 'UserName' in session:
                 continue
             if session['UserName'] != self.username:
                 continue
 
-            if not 'NowPlayingItem' in session:
-                return (False, None, None, None)
+            if 'NowPlayingItem' in session:
+                current_session = session['NowPlayingItem']
+                return {
+                    "status": ACTIVE,
+                    "state": PAUSED if session['PlayState']['IsPaused'] else PLAYING,
+                    "file_name": current_session['Name'],
+                    "view_offset": int(session['PlayState']['PositionTicks']/10000)
+                }
 
-            current_session = session['NowPlayingItem']
-            return (
-                True,
-                session['PlayState']['IsPaused'],
-                current_session['Name'],
-                int(session['PlayState']['PositionTicks']/10000)
-            )
+        info["status"] = IDLE
+        return info
