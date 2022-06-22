ed193fb7bbe2adc59655bcfecf164db0cef371d0
imsamuka <imsamuka@gmail.com>
2022-06-22 12:16:08 -0300

    add episode_missing hook example file

---
+++ hooks/episode_missing_ani-cli.py
@@ -0,0 +1,23 @@
+"""
+ Author: imsamuka
+ Place under ~/.config/trackma/hooks/ or ~/.trackma/hooks/
+
+ When a episode is not found, it executes 'ani-cli' to find and
+ watch it via streaming automatically!
+"""
+
+import shutil
+from trackma import utils
+
+# Executed when trying to watch an episode that don't exists on your library
+def episode_missing(engine, show, episode):
+
+    query = show["title"].strip()
+    anicli = shutil.which("ani-cli") # find 'ani-cli' executable
+    if anicli:
+        args = [anicli, "-q", "best", "-a", str(episode), query]
+        cmd = " ".join(args[:-1]) + f" '{query}'"
+        engine.msg.info("episode_missing", cmd) # Show the command used
+        utils.spawn_process(args)
+    else:
+        engine.msg.info("episode_missing", "ani-cli was not found")
