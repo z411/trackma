4e642d5f72ce84306b354c2d86a4650cdbcf4ce9
Jafar Abbas <s.jafar25462@gmail.com>
2023-02-05 13:46:34 +0530

    Show next airing episode (if any)

--- trackma/lib/libanilist.py
+++ trackma/lib/libanilist.py
@@ -430,6 +430,7 @@ fragment mediaListEntry on MediaList {
       averageScore
       chapters episodes
       status
+      nextAiringEpisode { airingAt episode }
       startDate { year month day }
       endDate { year month day }
       siteUrl
@@ -457,7 +458,7 @@ fragment mediaListEntry on MediaList {
         info = utils.show()
         showid = item['id']
 
-        info.update({
+        show_info = {
             'id': showid,
             'title': item['title']['userPreferred'],
             'total': self._c(item[self.total_str]),
@@ -483,6 +484,13 @@ fragment mediaListEntry on MediaList {
                 ('Status',          self._translate_status(item['status'])),
             ]
         })
+        if item.get('nextAiringEpisode', None):
+            next_episode_release_time = self._int2date(item.get('nextAiringEpisode').get('airingAt'))
+            time_diff  = next_episode_release_time - datetime.datetime.now()
+            show_info['extra'].append(('Next Episode', item.get('nextAiringEpisode').get('episode')))
+            show_info['extra'].append(('Next Episode In', str(time_diff)))
+        show_info['extra'].append(('Visible data fetched on', datetime.datetime.now().strftime("%d %B, %Y at %I:%M %p")))
+        info.update(show_info)
         return info
 
     def _apply_scoreformat(self, fmt):
