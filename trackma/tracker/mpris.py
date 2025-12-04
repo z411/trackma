z411/trackma

commit e922f456f1ea30b5328485567a55562c02c2c9bd

mpris: Fix tracker status not resetting on unrecognized show
A condition was missing that updated the tracker status when the active
player status changed to an unrecognized video, meaning that playing
that unrecognized video would cause an update for the previously
playing episode, if not already updated.

trackma/tracker/mpris.py
@@ -328,11 +328,11 @@ class MprisTracker(tracker.TrackerBase):
                 self.last_filename = previous_last_filename
             return False
 
-        if new_show:
+        if not is_new_player or new_show:
             self.msg.debug(f"New tracker status: {state} (previously: {self.last_state})")
             self.update_show_if_needed(state, show_tuple)
 
-        if is_new_player and new_show:
+        if is_new_player:
             self.msg.debug(f"Setting active player: {player.wellknown_name}")
             self.active_player = player
 

commit cf260270e85fb503c4645cef62c20646499e1b8d

mpris: Fix tracker state updating
Updates for the current player that did not result in a new file being
played would cause the tracker state to be set to `None`, resulting in
the tracker to not update the currently watching episode when the timer
expires.

Fixes a regression from e922f456f1ea30b5328485567a55562c02c2c9bd.

trackma/tracker/mpris.py
@@ -330,13 +330,13 @@ class MprisTracker(tracker.TrackerBase):
                 self.last_filename = previous_last_filename
             return False
 
-        if not is_new_player or new_show:
-            self.msg.debug(f"New tracker status: {state} (previously: {self.last_state})")
-            self.update_show_if_needed(state, show_tuple)
-
-        if is_new_player:
+        if is_new_player and new_show:
             self.msg.debug(f"Setting active player: {player.wellknown_name}")
             self.active_player = player
+
+        if state:
+            self.msg.debug(f"New tracker status: {state} (previously: {self.last_state})")
+            self.update_show_if_needed(state, show_tuple)
 
         if player.playback_status == PlaybackStatus.PLAYING:
             self._start_timer()