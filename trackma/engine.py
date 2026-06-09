11b8ef1faa78c3a0bc681ae4ec0b344adf91e8f5
FichteFoll <fichtefoll2@googlemail.com>
2026-06-09 23:30:36 +0200

    engine: Add some type hints

--- trackma/engine.py
+++ trackma/engine.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+from __future__ import annotations
 
 import datetime
 import os
@@ -25,12 +26,16 @@ import time
 from decimal import Decimal
 from functools import lru_cache, partial
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from trackma import data
 from trackma import messenger
 from trackma import utils
 from trackma.extras import redirections
 from trackma.parser import get_parser_class
+
+if TYPE_CHECKING:
+    from .tracker import TrackerBase
 
 
 class Engine:
@@ -50,11 +55,11 @@ class Engine:
     The **message_handler** is a reference to a messaging function for the engine
     to send to. Optional.
     """
-    data_handler = None
-    tracker = None
+    data_handler: data.Data
+    tracker: TrackerBase | None = None
     redirections = None
     config = {}
-    msg = None
+    msg: messenger.Messenger
     loaded = False
     playing = False
     hooks_available = []
